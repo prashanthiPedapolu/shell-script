55588669bc9db57aa98cc447fcb152ed3707ec48
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-01 16:53:56 -0400

    conversation

--- variable.sh
+++ variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-$value1 = trump
-$value2 = musk
+$value1=trump
+$value2=musk
 
-echo "$value1: hey $value2 how are you"
-echo "$value2: hi $value1 im doing good ,how about you "
-echo "hey  $value2 we have a coffe outside"
-echo musk: "no $value1  im bit busy today shall we meet tomorro "
+echo "$value1:: hey $value2 how are you"
+echo "$value2:: hi $value1 im doing good ,how about you "
+echo $value1::"hey  $value2 we have a coffe outside"
+echo $value2: :"no $value1  im bit busy today shall we meet tomorro "
