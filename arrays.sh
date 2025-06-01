470f764aacdb0015674378f309a88d5a8e009aa9
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-01 17:35:15 -0400

    arrays

--- arrays.sh
+++ arrays.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 MOVIES={"hit1","hit2","hit3","hit4","hit5","rrr"}
-echo "all movies: ${MOVIES[@/]}"
+echo "all movies: ${MOVIES[0]}"
+echo  "all movies : ${MOVIES[1]}"
+echo  "all movies : ${MOVIES[2]}"
+echo  "all movies : ${MOVIES[3]}"
+echo  "all movies : ${MOVIES[4]}"
