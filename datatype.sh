ea9cc9104bf15314dcd9fd779d7d9de5df8bf834
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-01 17:21:16 -0400

    sum two numbers

--- datatype.sh
+++ datatype.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-NAME=100
-AGE=200
-TIMESTAMP=$((date))
-echo "script executed at $TIMESTAMP"
-SUM=$(($NAME+$AGE))
 
-echo "SUM of $NAME and $AGE is: $SUM"
+# this is comment
+NUMBER1=100
+NUMBER2=200
+
+TIMESTAMP=$(date)
+echo "Script executed at: $TIMESTAMP"
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
