7014dabcd0536a88b7d12ef7c82cac4181eb7216
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-01 18:16:26 -0400

    conditions

--- conditions.sh
+++ conditions.sh
@@ -3,6 +3,6 @@ if [ $NUMBER -lt 10 ]
 then
 echo "given number is $NUMBER is less than 10"
 else
-echo "given number is $NAME is not less than 10"
+echo "given number is $Number is not less than 10"
 exit 1
 fi
