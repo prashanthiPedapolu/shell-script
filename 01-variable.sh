f36288d8c1c9181bc04be59d5e664b7d05d2840f
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-01 17:01:03 -0400

    conversation

--- 01-variable.sh
+++ 01-variable.sh
@@ -3,6 +3,9 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1:: Hey $PERSON2, How are you?"
+# There is a typo in the word "Nohting" in the line:
+# echo "$PERSON2:: Nohting, just going to Mars now, will you come?"
+# It should be "Nothing".
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
 echo "$PERSON1:: I am fine too. What's up?"
 echo "$PERSON2:: Nohting, just going to Mars now, will you come?"
