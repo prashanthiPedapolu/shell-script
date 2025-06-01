eee0679fe5e9cc56750cf8f9d2010211f24d06c4
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-01 18:55:28 -0400

    installing mysql

--- server.sh
+++ server.sh
@@ -8,11 +8,11 @@ echo "you have root access to continue"
 fi
 
 dnf list installed mysql
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then 
 echo "mysql does not exist please install"
 dnf install mysql -y
-if[ $? eq 0 ]
+if [ $? eq 0 ]
 then
 echo "mysql is installed alredy success"
 else
