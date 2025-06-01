182ff3a6b9b22814708e0a6fa8f06cea16d5a723
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-01 17:45:43 -0400

    arrays

--- arrays.sh
+++ arrays.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MOVIES={"hit1" "hit2" "hit3" "hit4" "hit5" "rrr"}
+MOVIES=("hit1" "hit2" "hit3" "hit4" "hit5" "rrr")
 echo "all movies: ${MOVIES[0]}"
 echo "all movies: ${MOVIES[1]}"
 echo "all movies: ${MOVIES[2]}"
