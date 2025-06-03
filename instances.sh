a77b859eb2bb9a1088057ca01c617ec8e3754b8d
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-02 22:45:21 -0400

    installing instances

--- instances.sh
+++ instances.sh
@@ -8,3 +8,12 @@ for instance in "${INSTANCES[@]}"; do
 	echo "Creating instance for $instance"
 	aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0bd9e46d0f3339274 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].PrivateIpAddress" --output text
 	done
+	if [ $instance ! = "frontend" ]
+	then
+	aws ec2 describe-instances --instance-id i-020f9fbd4c5f9f2c7 --query "Reservations[0].Instances[0].PrivateIpAddress" --output text
+	else 
+	aws ec2 describe-instances --instance-ids i-020f9fbd4c5f9f2c7 --query "Reservations[0].Instances[0].PrivateIpAddress" --output text
+	fi
+
+
+
